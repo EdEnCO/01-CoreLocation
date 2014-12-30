af120bd6aef486a7c02f9a5ac6035b6430b434a0
Gianfranco <cotumaccio@gmail.com>
2014-12-30 15:06:39 +0100

    Adding CLLocationManagerDelegate

--- 01-CoreLocation/ViewController.swift
+++ 01-CoreLocation/ViewController.swift
@@ -28,6 +28,33 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
         
     }
+    
+    // MARK: - CLLocationManagerDelegate
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        println("ERROR: \(error.localizedDescription)")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        println("New Location: \(manager.location)")
+        
+        let currentLocation = locationManager.location
+        
+        if currentLocation != nil {
+            println("Latitude: \(currentLocation.coordinate.latitude)")
+            println("Longitude: \(currentLocation.coordinate.longitude)")
+            
+            geocoder.reverseGeocodeLocation(currentLocation, completionHandler: {
+                placemarks, error in
+                
+                if error != nil && placemarks.count > 0 {
+                    self.placeMark = placemarks.last as? CLPlacemark
+                    println("Placemark: \(self.placeMark?.country)")
+                    self.locationManager.stopUpdatingLocation()
+                }
+            })
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
