2301cb2ee924cebe2203b784cbee15177229b998
Gianfranco <cotumaccio@gmail.com>
2014-12-30 12:27:22 +0100

    Initial Commit

---
+++ 01-CoreLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  01-CoreLocation
+//
+//  Created by Gianfranco Cotumaccio on 30/12/14.
+//  Copyright (c) 2014 Edenco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
