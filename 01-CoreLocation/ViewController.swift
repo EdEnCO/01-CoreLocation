af5b40471955e5dfc6e72cdd76488482af1ba5c6
GCotumaccio <cotumaccio@gmail.com>
2016-06-23 15:08:11 +0200

    First Commit

--- 01-CoreLocation/ViewController.swift
+++ 01-CoreLocation/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import CoreLocation
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    // MARK: - Variables
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
     }
-
+    
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("ERROR: \(error.localizedDescription)")
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Location: \(manager.location)")
+        
+        let currentLocation = locationManager.location
+        
+        if currentLocation != nil {
+            let lat = (currentLocation?.coordinate.latitude)!
+            let lon = (currentLocation?.coordinate.longitude)!
+            print("Latitude: \(lat) - Longitude: \(lon)")
+        }
+        
+        manager.stopUpdatingLocation()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
